116d4323b19c2d8324bb2fe0d012f27892419de3
John James Libron <johnjames.libron@gmail.com>
2025-12-16 17:30:25 +0800

    Initial Spring Boot project

---
+++ src/main/java/com/onehub/employee/OneHubEmployeeApplication.java
@@ -0,0 +1,12 @@
+
+package com.onehub.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneHubEmployeeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OneHubEmployeeApplication.class, args);
+    }
+}
